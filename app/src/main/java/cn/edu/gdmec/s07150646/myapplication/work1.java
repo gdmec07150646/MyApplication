802c2a9eb8b0be1eddb390b350cb2d29f398dad6
gdmec07150646 <865532471@qq.com>
2016-09-06 13:02:00 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150646/myapplication/work1.java
@@ -0,0 +1,13 @@
+package cn.edu.gdmec.s07150646.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class work1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work1);
+    }
+}
